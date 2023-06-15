31f0f2805f76d4dc72c2c6afc0f43f707dccb819
sein <lsi6601@gmail.com>
2023-06-15 15:05:46 +0900

    login

--- src/main/java/com/playdata/todos/dao/UserDao.java
+++ src/main/java/com/playdata/todos/dao/UserDao.java
@@ -1,28 +1,56 @@
 package com.playdata.todos.dao;
-
-import com.mysql.cj.jdbc.ConnectionImpl;
 import com.playdata.todos.config.JdbcConnection;
 import com.playdata.todos.dto.User;
-
+import javax.jws.soap.SOAPBinding;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-public class UserDao { //DataaccessObject
+import java.util.ArrayList;
+import java.util.List;
+public class UserDao {
     public void insert(User user){
-        Connection conn  = new JdbcConnection().getJdbc();
-
-        String sql = "insert into suer(username, password, name) " + "Values(?,?,?)";
-
-        try{
-            PreparedStatement pst = conn.prepareStatement(sql);
-            pst.setString(1, user.getUsername());
-            pst.setString(2, user.getPassword());
-            pst.setString(3, user.getName());
-            pst.executeUpdate();
+        Connection conn = new JdbcConnection().getJdbc();
+        String sql = "insert into users(username, password, name) values (?, ?, ?);";
+        try {
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            psmt.setString(1, user.getUsername());
+            psmt.setString(2, user.getPassword());
+            psmt.setString(3, user.getName());
+            psmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
+    public boolean login(String username, String password){
+        List<User> users = new ArrayList<User>();
+        Connection conn = new JdbcConnection().getJdbc();
+        String sql = "select * from users where username = ? and password = ?";
+        try {
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            psmt.setString(1, username);
+            psmt.setString(2, password);
+            ResultSet resultSet = psmt.executeQuery();
+            while (resultSet.next()){
+                users.add(makeUser(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users.size() != 0;
+    }
+    private User makeUser(ResultSet resultSet){
+        try {
+            int id1 = resultSet.getInt("id");
+            String password = resultSet.getString("password");
+            String username = resultSet.getString("username");
+            String name = resultSet.getString("name");
+            String createAt = resultSet.getString("create_at");
+            return new User(id1, username, name, password, createAt);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
