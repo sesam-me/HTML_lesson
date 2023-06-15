3b2ec4f00a537bbbefb8cf5975c016006ff4e230
sein <lsi6601@gmail.com>
2023-06-15 10:10:46 +0900

    user page

---
+++ src/main/java/com/playdata/todos/config/jdbcConnection.java
@@ -0,0 +1,30 @@
+package com.playdata.todos.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class jdbcConnection {
+
+        private final String url = "jdbc:mysql://localhost:3306/test" + "?serverTimezone=Asia/Seoul&characterEncoding=UTF-8";
+        private final String root = "dm";
+        private final String password = "1234";
+
+
+        public Connection getJdbc() {
+            Connection conn;
+
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                conn = DriverManager.getConnection(url, root, password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            return conn;
+        }
+}
+
